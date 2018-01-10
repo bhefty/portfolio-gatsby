be3e900658757b3285cf6f7d8cc1cc5220c7a18e
bhefty <billhefty@gmail.com>
2018-01-09 22:52:47 -0600

    move header to separate component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,39 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
+
+import MainHeader from './MainHeader/MainHeader'
 
 import 'sanitize.css/sanitize.css'
 import './global-styles'
-
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem'
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem'
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to='/'
-          style={{
-            color: 'white',
-            textDecoration: 'none'
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -44,7 +16,7 @@ const TemplateWrapper = ({ children }) => (
         { name: 'keywords', content: 'sample, something' }
       ]}
     />
-    <Header />
+    <MainHeader />
     <div
       style={{
         margin: '0 auto',
