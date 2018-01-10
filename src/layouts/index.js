dcb5fdeaa5f4aed934ee538b9bbe202ce11d7354
bhefty <billhefty@gmail.com>
2018-01-09 21:55:17 -0600

    load styles from styled-component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import './index.css'
+import 'sanitize.css/sanitize.css'
+import './global-styles'
 
 const Header = () => (
   <div
