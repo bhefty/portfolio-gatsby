56af47d2f45be985e81ff24569b92d687521c456
bhefty <billhefty@gmail.com>
2018-01-11 00:23:31 -0600

    add project section

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import MainHeader from '../components/MainHeader/MainHeader'
 import About from '../components/About/About'
+import ProjectsSection from '../components/ProjectsSection/ProjectsSection'
 
 const IndexPage = () => (
   <div>
     <MainHeader />
     <About />
+    <ProjectsSection />
   </div>
 )
 
