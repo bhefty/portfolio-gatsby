d07febb5d0bcd89d26f41de2af5d4674872430f0
bhefty <billhefty@gmail.com>
2018-01-13 21:35:14 -0600

    add plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`
+    title: `Bill Hefty Portfolio`
   },
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-styled-components`]
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src`
+      }
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`
+  ]
 }
