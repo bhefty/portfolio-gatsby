5f86e8ce6d7694cdfe1f829e0c73a39d6bc8d3ef
bhefty <billhefty@gmail.com>
2018-01-13 21:34:21 -0600

    add blog post template

---
+++ src/templates/blog-post.js
@@ -0,0 +1,68 @@
+/* global graphql */
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledContainer = styled.main`
+  padding-bottom: 50px;
+
+  .img-wrapper {
+    height: 40vh;
+
+    img {
+      object-fit: cover;
+      height: 100%;
+      width: 100%;
+    }
+  }
+
+  .date {
+    color: #a0a6ab;
+    letter-spacing: 2px;
+    margin-top: 1em;
+    text-align: center;
+  }
+  
+  .title {
+    color: #555;
+    margin: 0.8em auto;
+    max-width: 1000px;
+    padding: 0 20px;
+  }
+
+  .content {
+    padding: 0 20px;
+  }
+`
+
+export default ({ data }) => {
+  const post = data.markdownRemark
+  return (
+    <StyledContainer>
+      <div className='img-wrapper'>
+        <img src={post.frontmatter.image.childImageSharp.sizes.src} alt={post.frontmatter.title} />
+      </div>
+      <p className='date'>{post.frontmatter.date}</p>
+      <h1 className='title'>{post.frontmatter.title}</h1>
+      <div className='content' dangerouslySetInnerHTML={{ __html: post.html }} />
+    </StyledContainer>
+  )
+}
+
+export const query = graphql`
+  query BlogPostQuery($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        date(formatString: "MMM DD, YYYY")
+        image {
+          childImageSharp {
+            sizes {
+              src
+            }
+          }
+        }
+        title
+      }
+      html
+    }
+  }
+`
