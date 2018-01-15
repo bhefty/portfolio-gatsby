51dc2c8b7dff28a91d9664a2aaba248607b51f57
bhefty <billhefty@gmail.com>
2018-01-14 21:56:44 -0600

    create colors

---
+++ src/layouts/colors.js
@@ -0,0 +1,9 @@
+export const colors = {
+  primary: '#292E38',
+  lightShade: '#E9E6D9',
+  lightAccent: '#77938D',
+  darkAccent: '#5472A1',
+  darkShade: '#1B1720',
+  whiteLight: '#ECEFF4',
+  whiteAlt: '#FBFBFB'
+}
